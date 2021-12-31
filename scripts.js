1f18a83d955d27fdf4e9d806c682a90f78cc6435
Bridgett Taylor <btaylor7224@gmail.com>
2021-12-31 15:47:40 -0800

    the progress bar is responding to click and mousedown events properly - need to add a full screen icon and functionality next

--- scripts.js
+++ scripts.js
@@ -27,9 +27,41 @@ function skip() {
   video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+
+  //   console.log(this.value);
+  //   console.log(this.name);
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+  //   console.log(e);
+}
+
 // Set up event listeners
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateBtn);
 video.addEventListener("pause", updateBtn);
+video.addEventListener("timeupdate", handleProgress);
 toggle.addEventListener("click", togglePlay);
 skipBtns.forEach((button) => button.addEventListener("click", skip));
+ranges.forEach((range) => range.addEventListener("click", handleRangeUpdate));
+ranges.forEach((range) =>
+  range.addEventListener("mousemove", handleRangeUpdate)
+);
+
+let mousedown = false;
+
+progress.addEventListener("click", scrub);
+// first check for mousedown - if true run scrub
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mouseup", () => (mousedown = false));
